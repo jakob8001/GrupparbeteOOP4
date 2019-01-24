5428224b41af11f8791d79d0f5d247de5078a9b3
jakob8001 <jakobn04@gmail.com>
2019-01-24 15:12:46 +0100

    extend Volvo740

---
+++ src/Volvo7402.java
@@ -0,0 +1,21 @@
+import javafx.scene.paint.Color;
+
+public class Volvo7402 extends Car {
+
+	public final static double trimFactor = 1.25;
+
+	
+
+	public Volvo7402() {
+		color = Color.RED;
+		enginePower = 100;
+		modelName = "Volvo740";
+		stopEngine();
+	}
+
+
+	public double speedFactor() {
+		return enginePower * 0.01 * trimFactor;
+	}
+
+}
