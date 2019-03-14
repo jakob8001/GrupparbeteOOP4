d769ea442fd868cad95e8f46fc7617d4d3761ef8
jakob8001 <jakobn04@gmail.com>
2019-03-14 15:07:16 +0100

    javadoc

--- src/Movable.java
+++ src/Movable.java
@@ -1,7 +1,18 @@
 
 public interface Movable {
-
+	
+	/**
+	 * move() för att kunna flytta på bilen senare
+	 */
 	public void move();
+	
+	/**
+	 * Svänger vänster
+	 */
 	public void turnLeft();
+	
+	/**
+	 * Svänger höger
+	 */
 	public void turnRight();
 }
